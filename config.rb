508d40d155053293211649e85431687c53f304d4
Jamie English <jamienglish@gmail.com>
2011-10-24 21:35:22 +0100

    Got gallery pages hacked together, sorted nav out

--- config.rb
+++ config.rb
@@ -61,7 +61,6 @@ activate :directory_indexes
 
 # Methods defined in the helpers block are available in templates
 helpers do
-  # Calculate the years for a copyright
   def copyright_years(start_year)
     end_year = Date.today.year
     if start_year == end_year
@@ -72,7 +71,7 @@ helpers do
   end
 
   def current?(title, link)
-    ('current' if title == link)
+    'current' if @anchor == link or @title == link
   end
 
   def url(link)
@@ -110,22 +109,4 @@ configure :build do
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
-end
-
-Dir.glob "source/gallery/*" do |gallery|
-  if File.directory? gallery and not %w(. ..).include? gallery
-    gallery_items = []
-    Dir.glob "#{gallery}/*.jpg" do |img|
-      gallery_item = {:image => img}
-      markdown = "#{File.basename(img)}.markdown}"
-      if File.exists? markdown
-        gallery_item[:description] = Redcarpet.new(File.read(markdown)).to_html.html_safe
-      end
-      gallery_items << gallery_item
-    end
-    page "/gallery/#{gallery}.html", :proxy => "/gallery/template.html.haml" do
-      @title = "Gallery"
-      @gallery_items = gallery_items
-    end
-  end
 end
